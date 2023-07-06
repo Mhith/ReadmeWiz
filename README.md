95644dfca5f5753f1b4da124f2ac1aeff940ae9f
Mhith <dev.mhith@gmail.com>
2023-07-06 11:30:29 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ReadmeWiz
+# ReadmeWiz
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Homepage from './pages/Homepage'
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Homepage />} />
+    </Routes>
+
+    </>
+  )
+}
+
+export default App
